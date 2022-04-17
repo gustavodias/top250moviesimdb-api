519fab4ff67114e3fe6877186bca456b45f89646
gustavo.emerson_meta <gustavo.emerson_meta>
2022-04-17 19:00:34 -0300

    Setting project folders

---
+++ backend/src/main/java/dev/gustavdias/top250moviesimdbapi/Top250moviesimdbApiApplication.java
@@ -0,0 +1,13 @@
+package dev.gustavdias.top250moviesimdbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Top250moviesimdbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Top250moviesimdbApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/gustavdias/top250moviesimdbapi/Top250moviesimdbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gustavdias.top250moviesimdbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Top250moviesimdbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
